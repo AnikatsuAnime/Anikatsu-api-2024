1ac8f927688870515833ef01e071f7fb5b26653b
AnikatsuAnime <anikatsu.anime@gmail.com>
2024-09-10 13:30:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+// Import the api module
+const api = require('./lib/api');
+
+// If api.js exports a function
+api();
+
+// If api.js exports an object with functions or properties
+// api.someFunction();
